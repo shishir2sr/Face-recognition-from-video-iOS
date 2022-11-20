3523fd673d37d88c842c43b7ae5f31e65964ba85
Yasir Arefin Tusher <shishir2sr@users.noreply.github.com>
2022-11-20 07:54:13 +0600

    Initial Commit

---
+++ Smart Video Player with face reconition feature/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Smart Video Player with face reconition feature
+//
+//  Created by Yeasir Arefin Tusher on 20/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
